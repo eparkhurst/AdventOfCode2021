c628a2adf774e1174cee02f6b1b861c64a1e4369
Elijah Parkhurst <eli@bouncecon.com>
2023-07-26 17:33:04 -0600

    easy enough

--- day1/part2.py
+++ day1/part2.py
@@ -4,10 +4,12 @@ with open('input.txt') as f:
 input = [int(i) for i in content]
 
 increases = 0
-length = len(input)
+max = len(input) - 3
 for i, value in enumerate(input):
-    if(i<length -1):
-        if(value < input[i+1]):
+    if(i < max):
+        first_set = value + input[i+1] + input[i+2]
+        second_set = input[i+1] + input[i+2] + input[i+3]
+        if(first_set < second_set):
             increases += 1
 
 print(increases)
