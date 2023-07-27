591823aca2477d3d07ee3aad3eec381843962fe6
Elijah Parkhurst <eli@bouncecon.com>
2023-07-27 09:07:03 -0600

    completed day 2 and installed black as code formatter

--- day1/part1.py
+++ day1/part1.py
@@ -1,13 +1,13 @@
-with open('input.txt') as f:
-    content = f.read().strip().split('\n')
+with open("input.txt") as f:
+    content = f.read().strip().split("\n")
 
 input = [int(i) for i in content]
 
 increases = 0
 length = len(input)
 for i, value in enumerate(input):
-    if(i<length -1):
-        if(value < input[i+1]):
+    if i < length - 1:
+        if value < input[i + 1]:
             increases += 1
 
 print(increases)
--- day1/part2.py
+++ day1/part2.py
@@ -1,15 +1,15 @@
-with open('input.txt') as f:
-    content = f.read().strip().split('\n')
+with open("input.txt") as f:
+    content = f.read().strip().split("\n")
 
 input = [int(i) for i in content]
 
 increases = 0
 max = len(input) - 3
 for i, value in enumerate(input):
-    if(i < max):
-        first_set = value + input[i+1] + input[i+2]
-        second_set = input[i+1] + input[i+2] + input[i+3]
-        if(first_set < second_set):
+    if i < max:
+        first_set = value + input[i + 1] + input[i + 2]
+        second_set = input[i + 1] + input[i + 2] + input[i + 3]
+        if first_set < second_set:
             increases += 1
 
 print(increases)
