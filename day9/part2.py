7b3a7ce6e0b8ca1070d4dbae09a8911b3beb926f
Elijah Parkhurst <eli@bouncecon.com>
2023-08-02 10:20:06 -0600

    finished part2. it was pretty much just adding a bfs on top of part1

--- day9/part2.py
+++ day9/part2.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 with open("input.txt") as f:
     raw_input = f.read().strip().split("\n")
 
@@ -12,8 +14,28 @@ def get_point(x, y, matrix):
         return 10
 
 
+# bfs
+def get_basin(x, y, matrix):
+    count = 0
+    visited = set()
+    q = deque()
+    q.append((x, y))
+    while q:
+        x, y = q.popleft()
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        if get_point(x, y, matrix) < 9:
+            count += 1
+            q.append((x - 1, y))
+            q.append((x + 1, y))
+            q.append((x, y - 1))
+            q.append((x, y + 1))
+    return count
+
+
 def get_minima(matrix):
-    minima = []
+    basins = []
     for y, row in enumerate(matrix):
         for x, point in enumerate(row):
             surrounding = []
@@ -26,9 +48,10 @@ def get_minima(matrix):
                 print("how often?", point, x, y)
                 print(row)
             if point < lowest_surrounding:
-                minima.append(point)
+                basin = get_basin(x, y, matrix)
+                basins.append(basin)
 
-    return minima
+    return basins
 
 
 def main(input):
@@ -36,9 +59,12 @@ def main(input):
     for line in input:
         chars = [*line]
         matrix.append([int(c) for c in chars])
-    local_minima = get_minima(matrix)
-    count = sum([x + 1 for x in local_minima])
-    print(count)
+    basins = sorted(get_minima(matrix))
+    largest = basins[-3:]
+    total = 1
+    for i in largest:
+        total *= i
+    print(total)
 
 
 main(raw_input)
